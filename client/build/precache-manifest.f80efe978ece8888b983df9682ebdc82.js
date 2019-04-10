6fcb7cd39ab6c5c2f5b984c34a5071f2eff61f10
JoshSlater07 <joshslater07@gmail.com>
2019-04-10 14:37:04 +0100

    deploy

---
+++ client/build/precache-manifest.f80efe978ece8888b983df9682ebdc82.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "58559c1aa6313bcba888ea3b769ecb93",
+    "url": "/static/media/002-time.58559c1a.png"
+  },
+  {
+    "revision": "8b8b82b8562fa7bd469b",
+    "url": "/static/css/main.47627104.chunk.css"
+  },
+  {
+    "revision": "2995aa2e92301d2068ef08f8ca77e87c",
+    "url": "/static/media/backbutton.2995aa2e.png"
+  },
+  {
+    "revision": "fe7215b66f2b661f816f",
+    "url": "/static/js/1.fe7215b6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "93a32ca501246bb8546c135de60a3c18",
+    "url": "/static/media/logofinal.93a32ca5.png"
+  },
+  {
+    "revision": "0b082b8543da46d147659bd49ebb114e",
+    "url": "/static/media/tempalte.0b082b85.jpg"
+  },
+  {
+    "revision": "8b8b82b8562fa7bd469b",
+    "url": "/static/js/main.8b8b82b8.chunk.js"
+  },
+  {
+    "revision": "4b9584b58ece7bcf090b131d2429b903",
+    "url": "/static/media/003-family.4b9584b5.png"
+  },
+  {
+    "revision": "686a482776823a23da96be5146de1a27",
+    "url": "/static/media/001-system.686a4827.png"
+  },
+  {
+    "revision": "8ba040a03e06b21440ef7f777cdea88d",
+    "url": "/static/media/timeedit.8ba040a0.png"
+  },
+  {
+    "revision": "79edde2a93dbe0027ed506cbb7d3e1cb",
+    "url": "/static/media/back2.79edde2a.png"
+  },
+  {
+    "revision": "fe7215b66f2b661f816f",
+    "url": "/static/css/1.e1cda36f.chunk.css"
+  },
+  {
+    "revision": "b5990c88d01119e5f83a93474b0f1ca4",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const timelines = require("./routes/api/timelines")
+const port = process.env.PORT || 5000;
 const app = express();
 // Bodyparser middleware
 app.use(
@@ -29,5 +30,21 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/timelines", timelines);
-const port = process.env.PORT || 5000;
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
